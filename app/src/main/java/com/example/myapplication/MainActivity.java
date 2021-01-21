bcb2be87b321b1e43ee10890084163d8dbeb3982
Евгений Дамирович <9058073004j@gmail.com>
2021-01-21 15:53:21 +0500

    Добавил логику в кнопки, осталось запустить бегущие циферки, возможно в другом потоке...

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,16 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import java.sql.Time;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +19,12 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonStop;
     private Button buttonClear;
     private EditText screenTimer;
-    private Date counter = new Date() ;
+    private SimpleDateFormat formatter = new SimpleDateFormat("mm:ss:SS");    //создаю форматер дату, с помощью которого буду выводить время
+//    private Date currentTime;                                              //переменная типо Дата указывающая текущее время
+    private long counterStart;                                            //создаю переменную для начала отсчета
+    private long counterStop ;                                                          //создаю переменную для конца отсчета
+    private long counter = 0;                                                         //перемменная для подсчета времени в лонгах
+
 
 
 
@@ -30,13 +38,13 @@ public class MainActivity extends AppCompatActivity {
         buttonClear = findViewById(R.id.buttonClear);
         screenTimer = findViewById(R.id.editTextTime);
 
-
-        screenTimer.setText(counter.toString());
+        screenTimer.setText(formatter.format(counter));
 
         View.OnClickListener startOnClickListener = new View.OnClickListener() {  //создаю лисенер для кнопки Старт
             @Override
             public void onClick(View v) {
-                screenTimer.setText("нажали старт");
+                    counterStart = new Date().getTime() - counter;
+                    //screenTimer.setText("нажали старт");
             }
         };
         buttonStart.setOnClickListener(startOnClickListener); // вешаю лисенер на кнопку Старт
@@ -44,7 +52,11 @@ public class MainActivity extends AppCompatActivity {
         View.OnClickListener stopOnClickListener = new View.OnClickListener() {  //создаю лисенер для кнопки Стоп
             @Override
             public void onClick(View v) {
-                screenTimer.setText("нажали стоп");
+                counterStop = new Date().getTime();
+                counter = counterStop - counterStart;
+                screenTimer.setText(formatter.format(counter));
+
+                //screenTimer.setText("нажали стоп");
             }
         };
         buttonStop.setOnClickListener(stopOnClickListener); // вешаю лисенер на кнопку Стоп
@@ -52,8 +64,9 @@ public class MainActivity extends AppCompatActivity {
         View.OnClickListener clearOnClickListener = new View.OnClickListener() {  //создаю лисенер для кнопки Клеар
             @Override
             public void onClick(View v) {
-
-                screenTimer.setText("нажали очистить");
+                counter = 0;
+                screenTimer.setText(formatter.format(counter));
+                //screenTimer.setText("нажали очистить");
             }
         };
         buttonClear.setOnClickListener(clearOnClickListener); // вешаю лисенер на кнопку Клеар
