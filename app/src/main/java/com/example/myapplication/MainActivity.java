453130aea379c7f0455d8fdb6d0c971b117d97d1
Leonid <TheLeonid412@gmail.com>
2021-01-27 00:16:38 +0500

    добавил процедуры для кнопок

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,15 +3,37 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int secCount;
+    private boolean isActivate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button1 = (Button)findViewById(R.id.button1);
+        activateTimer();
+    }
+
+    private void activateTimer(){
+        final TextView textView = (TextView)findViewById(R.id.textView);
+    }
+
+    public void startOnClick(View view){
+        isActivate = true;
+    }
+
+    public void stopOnClick(View view){
+        isActivate = false;
+    }
+
+    public void resetOnClick(View view){
+        isActivate = false;
+        secCount = 0;
     }
 }
